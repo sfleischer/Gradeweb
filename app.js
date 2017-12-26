b91901b5e1791b36a385237acfbcb2a068e4bf48
sfleischer <fls@seas.upenn.edu>
2017-12-25 22:00:38 -1000

    updates

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var web = require('./routes/web')
 
 var exphbs  = require('express-handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}));
+app.engine('.html', exphbs({ defaultLayout: 'main', extname: '.html'}));
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', '.hbs');
+app.set('view engine', '.html');
 
 app.use(web);
 app.get('/', function(req, res) {
