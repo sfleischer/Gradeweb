0c854430c978d011bbe61d3a65f48837b64abe92
sfleischer <fls@seas.upenn.edu>
2017-12-26 14:50:02 -1000

    working post

--- app.js
+++ app.js
@@ -2,13 +2,27 @@ var express = require('express')
 var app = express();
 var router = express.Router()
 var path = require('path');
-var web = require('./routes/web')
+var web = require('./routes/web');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true, limit: '100mb' }));
 
 var exphbs  = require('express-handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('.html', exphbs({ defaultLayout: 'main', extname: '.html'}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', '.html');
+
+var $ = require('jquery')(require('jsdom-no-contextify').jsdom().parentWindow);
+
+// Support for Cross-domain request with using jQuery
+// See: http://garajeando.blogspot.jp/2012/06/avoiding-xmlhttprequest-problem-using.html
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+$.support.cors = true;
+$.ajaxSettings.xhr = function () {
+	return new XMLHttpRequest;
+}
 
 app.use(web);
 app.get('/', function(req, res) {
--- routes/web.js
+++ routes/web.js
@@ -2,7 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
+	
 	res.render('index');
+});
+
+router.post('/', function(req, res){
+	var dakine = {'msg': "hi"};
+	console.log('req min', req.body.min);
+	res.send(req.body)
 })
+
 
 module.exports = router;
